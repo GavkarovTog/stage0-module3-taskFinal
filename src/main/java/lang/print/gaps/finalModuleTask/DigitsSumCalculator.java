c51d46a24e580f98425417afcd0556f29cdf798f
Tog <tog.gavk@gmail.com>
2024-05-25 20:24:59 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String strRepr = number + "";
+
+        System.out.println(strRepr.charAt(0) + strRepr.charAt(1) + strRepr.charAt(2) + strRepr.charAt(3));
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        StringBuilder builder = new StringBuilder(number + "");
+        builder.reverse();
+
+        System.out.println(builder);
     }
 }
